01a045351c6f9d5d28d8947f1fcaa2fb9008c8bc
ismael <>
2017-05-05 12:26:01 +0200

    commit

---
+++ ConflictosGit/src/conflictosgit/ConflictosGit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conflictosgit;
+
+/**
+ *
+ * @author ismael
+ */
+public class ConflictosGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Escrito por Ismael");
+    }
+    
+}
