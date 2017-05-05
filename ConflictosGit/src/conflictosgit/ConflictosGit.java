b37d663af0fa9f150c7e0e28d3c42912c21e4b6e
Andrea <>
2017-05-05 12:35:11 +0200

    Añadida linea Andrea

--- ConflictosGit/src/conflictosgit/ConflictosGit.java
+++ ConflictosGit/src/conflictosgit/ConflictosGit.java
@@ -17,6 +17,7 @@ public class ConflictosGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Escrito por Ismael");
+        System.out.println("Linea añadida por Andrea");
     }
     
 }
